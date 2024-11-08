40196e482d86f5e519b09328ec7dd30959865d33
Leonardo Rojas Escarez <arens.rojas@gmail.com>
2024-11-07 21:22:05 -0300

    asd
    asd

--- django_pets/citas_app/views.py
+++ django_pets/citas_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from openpyxl import Workbook
+from reportlab.lib.pagesizes import A4  # <---- Libreria
+from reportlab.pdfgen import canvas  # <---- Libreria
 import datetime
 # Create your views here.
 
@@ -43,3 +45,28 @@ def v_reporte(request):
     workbook.save(response)
 
     return response
+
+
+
+def v_generar_pdf(request): #<----- View
+    # Crear la respuesta HTTP con el tipo de contenido para PDF
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="reporte.pdf"'
+
+    # Crear el objeto canvas de ReportLab para generar el PDF
+    p = canvas.Canvas(response, pagesize=A4)
+    ancho, alto = A4  # Tamaño de la página
+
+    # Agregar contenido al PDF
+    p.setFont("Helvetica", 12)
+    p.drawString(100, alto - 100, "Hola, este es un PDF generado desde Django.")
+    p.drawString(100, alto - 120, "Puedes personalizar este contenido como desees.")
+
+    # Finalizar y cerrar el PDF
+    p.showPage()
+    p.save()
+
+    return response
+
+    
+    
