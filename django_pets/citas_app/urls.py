df1cd110e6d486fbe2377bfea85dcaf48d230abc
Leonardo Rojas Escarez <arens.rojas@gmail.com>
2024-11-07 22:19:01 -0300

    cambios

--- django_pets/citas_app/urls.py
+++ django_pets/citas_app/urls.py
@@ -4,6 +4,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("nueva", views.v_nueva, name="nueva"),
     path("lista", views.v_lista, name="lista"),
-    path("reporte_xls", views.v_reporte, name="reporte_xls"),
-    path("reporte_pdf", views.v_generar_pdf, name="reporte_pdf"),
+    path("reporte.xls", views.v_reporte, name="reporte_xls"),
+    path("reporte.pdf", views.v_generar_pdf, name="reporte_pdf"),
 ]
